692acd390e9c87aca84be58416eb674cce00b55b
chenhailong <chenhailong@powerlaw.ai>
2024-07-06 07:34:25 +0800

    feat(gormbase):query by ID

--- modelbase.go
+++ modelbase.go
@@ -3,7 +3,9 @@ package gormx
 import (
 	"reflect"
 
+	"github.com/pkg/errors"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 type ModelBase struct {
@@ -15,4 +17,57 @@ func (m *ModelBase) GetByID(db *gorm.DB, id int64) (DataObjecter, error) {
 	for dataObjectType.Kind() == reflect.Ptr {
 		dataObjectType = dataObjectType.Elem()
 	}
+
+	dataObjecterValue := reflect.New(dataObjectType)
+	result := dataObjecterValue.Interface().(DataObjecter)
+
+	if dataObjecterValue.Elem().Kind() == reflect.Struct {
+		doerField := dataObjecterValue.Elem().FieldByName("DataObjecter")
+		if doerField.IsValid() && doerField.CanSet() {
+			doerField.Set(dataObjecterValue)
+		}
+	}
+
+	if err := db.Take(result, id).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, errors.WithStack(err)
+	}
+	return result, nil
+}
+
+func (m *ModelBase) GetByIDWithLock(db *gorm.DB, id int64, lock Lock) (DataObjecter, error) {
+	// 1.生成该对象
+	dataObjecterType := reflect.TypeOf(m.DataObjecter)
+	for dataObjecterType.Kind() == reflect.Ptr {
+		dataObjecterType = dataObjecterType.Elem()
+	}
+	dataObjecterValue := reflect.New(dataObjecterType)
+	result := dataObjecterValue.Interface().(DataObjecter)
+
+	// 2.为生成的对象设置 DataObjecter 值
+	if dataObjecterValue.Elem().Kind() == reflect.Struct {
+		doerField := dataObjecterValue.Elem().FieldByName("DataObjecter")
+		if doerField.IsValid() && doerField.CanSet() {
+			doerField.Set(dataObjecterValue)
+		}
+	}
+
+	// 3.查找该对象
+	query := db
+	switch lock {
+	case NoLock:
+	case IS:
+		query = query.Clauses(clause.Locking{Strength: "SHARE"})
+	case IX:
+		query = query.Clauses(clause.Locking{Strength: "UPDATE"})
+	}
+	if err := query.Take(result, id).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, errors.WithStack(err)
+	}
+	return result, nil
 }
